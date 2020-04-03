212b828b508c8ea48d077413f4ed6aa513d0a74f
Fenil Parmar <47125158+11fenil11@users.noreply.github.com>
2020-04-04 04:20:38 +0530

    Update 3Day_Maximum Subarray.py

--- 3Day_Maximum Subarray.py
+++ 3Day_Maximum Subarray.py
@@ -9,6 +9,8 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 Follow up:
 
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+I have used KADANE's algorithm. You can find that on : https://youtu.be/86CQq3pKSUw
 """
 
 
