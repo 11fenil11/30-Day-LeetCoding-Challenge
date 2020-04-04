f7ce4a62a81a32b44dd5d4e475bbd0ec39c3fda8
Fenil Parmar <47125158+11fenil11@users.noreply.github.com>
2020-04-05 05:17:58 +0530

    Create 4Day Move Zero.py

---
+++ 4Day Move Zero.py
@@ -0,0 +1,26 @@
+"""
+Move Zeroes
+Solution
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+"""
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if(i==0):
+                nums.remove(i)
+                nums.append(0)
+        
