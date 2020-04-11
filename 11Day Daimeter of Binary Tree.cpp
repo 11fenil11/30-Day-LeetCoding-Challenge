fc2cfd51a02f221bac35b0bce4ec77995d935a68
Fenil Parmar <47125158+11fenil11@users.noreply.github.com>
2020-04-12 02:19:18 +0530

    Create 11Day Daimeter of Binary Tree.cpp

---
+++ 11Day Daimeter of Binary Tree.cpp
@@ -0,0 +1,44 @@
+/*
+
+Diameter of Binary Tree
+Solution
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+Example:
+Given a binary tree
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int depth(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        return 1+max(depth(root->left),depth(root->right));
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root)
+            return 0;
+        int res=depth(root->left)+depth(root->right);
+        return max(res,max(diameterOfBinaryTree(root->left),diameterOfBinaryTree(root->right)));
+    }
+};
